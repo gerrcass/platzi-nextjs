01ce6ccc17767592bc9085a348a6b9e4746d148b
Gerardo Castillo <gerrcass@gmail.com>
2022-11-28 10:52:05 -0400

    Adding tailwindcss to the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app'
 import Layout from '@components/Layout/Layout'
 
+import '../styles/globals.css'
+
 export default function MyApp({ Component, pageProps }: AppProps) {
     return <Layout>
         <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,9 @@ const Home = () => {
 
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
             {productList.map((product) => (
                 <div key={product.id}>
                     <Link href={`product/${product.id}`}>{product.name}</Link>
