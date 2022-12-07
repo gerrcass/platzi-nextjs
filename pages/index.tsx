f2f03e57a80d246bcffc259a7a2323abb39f907f
Gerardo Castillo <gerrcass@gmail.com>
2022-12-07 01:45:10 -0400

    Implementing Server Side Rendering in Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,19 @@ import { useState, useEffect } from 'react'
 import Layout from '@components/Layout/Layout'
 import ProductList from '@components/ProductList/ProductList'
 
-const Home = () => {
-    const [productList, setProductList] = useState<TProduct[]>([])
+export const getServerSideProps = async () => {
+    const response = await fetch('https://platzi-avocado-nextjs-tau.vercel.app/api/avo')
+    const { data: productList }: TAPIAvoResponse = await response.json()
 
-    useEffect(() => {
-        window
-            .fetch('/api/avo')
-            .then((response) => response.json())
-            .then(({ data }: TAPIAvoResponse) => {
-                setProductList(data)
-            })
-    }, [])
+    return {
+        props: {
+            productList
+        }
+    }
 
+}
+
+const Home = ({ productList }: { productList: TProduct[] }) => {
     return (
         <Layout>
             <ProductList products={productList} />
